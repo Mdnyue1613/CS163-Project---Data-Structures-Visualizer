744790aead1ef27f6c14de3c364c3c8b7018dc29
Mdnyue1613 <myduyenmyduyen060706@gmail.com>
2025-03-14 11:54:33 +0700

    BOX

---
+++ header/InputBox.h
@@ -0,0 +1,20 @@
+// #include "raylib.h"
+
+// struct InputBox
+// {
+//     struct TextBox
+//     {
+//         Rectangle Box; // create a rectangle box
+//         bool isMove(); // return true if the mouse move to the box
+//         bool isClick(); // return true if we click the box
+//         void afterClick(); // 
+//     };
+//     struct OptionBox
+//     {
+//         Rectangle Box; // create a rectangle box
+//         bool isMove(); // return true if the mouse move to the box
+//         bool isClick(); // return true if we click the box
+//         void afterClick();
+//     };
+
+// };
