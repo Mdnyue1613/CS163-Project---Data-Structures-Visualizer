13bfb521b3bcfb0fc3d6d218043e32361fc7644e
Phát Hoàng Đức Hưng <hdhphat2421@apcs.fitus.edu.vn>
2025-03-15 07:39:02 +0700

    Put some comments

--- header/InputBox.h
+++ header/InputBox.h
@@ -10,11 +10,11 @@ struct TextBox
     int posNum_x, posNum_y;
     BOX box;
     bool isClick;
-    void setState();
-    void setTextBox(BOX box);
-    void afterMove();
-    void afterClick();
-    void draw();
+    void setState(); // update isClick
+    void setTextBox(BOX box); // set box
+    void afterMove(); // handle when the box covers the mouse
+    void afterClick(); // handle when the box is being chosen
+    void draw(); // update
 };
 
 // struct OptionBox
