97fcded5c67b029a8356594ed11d660f278f07c8
Tuanlinh1405 <buituanlinh63@gmail.com>
2025-03-18 22:41:55 +0700

    Update GUI.h

--- header/GUI.h
+++ header/GUI.h
@@ -1,12 +1,10 @@
 #include "raylib.h"
-#include "DS1.h"
 #include "DS3.h"
 #include <vector>
 
 using namespace std;
 
 struct GUI {
-    static bool isOpenSetting;
     static bool isOpenMenu;
     static bool isOpenDS1;
     static bool isOpenDS2;
