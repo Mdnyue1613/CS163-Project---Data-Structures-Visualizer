828c65d9e87009984b1d33b4c381bfa4ca215f88
Tuanlinh1405 <buituanlinh63@gmail.com>
2025-03-24 13:38:21 +0700

    merge

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -95,20 +95,11 @@ void GUI::drawDS2() {
 }
 
 void GUI::drawDS3() {
-    LTextBox insert;
-    ActionBox action;
-    AVL Tree;
-    for (int i = 0; i < 20; i++) {
-        Tree.TreeRoot = Tree.insertNode(i);
-    }
     DS3 ds3;
     while (GUI::isOpenDS3) {
         BeginDrawing();
         ClearBackground(WHITE);
         ds3.draw();
-        insert.draw();
-        action.draw();
-        Tree.draw();
         BACK();
         EndDrawing();
         if(WindowShouldClose()) {
